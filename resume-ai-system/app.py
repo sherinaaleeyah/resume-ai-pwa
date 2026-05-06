7df081bb7128c442b84cd2ba81893425b5ad776b
Sherina Aleeyah <sherinaaleeyah@Sherinas-MacBook-Pro.local>
2026-05-06 23:15:58 +0800

    Deleted google import

--- resume-ai-system/app.py
+++ resume-ai-system/app.py
@@ -5,7 +5,6 @@ import re
 import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from google import genai
 import json
 import os
 
